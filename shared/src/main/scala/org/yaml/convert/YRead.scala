c1af570784c6f9dbb5ff2a2cb140ca9e0ce0f65c
Alejandro Loose <alejandro.loose@mulesoft.com>
2019-05-16 10:09:53 -0300

    Minor change in error message

--- shared/src/main/scala/org/yaml/convert/YRead.scala
+++ shared/src/main/scala/org/yaml/convert/YRead.scala
@@ -166,7 +166,7 @@ object YRead {
   implicit object YScalarYRead extends YRead[YScalar] {
     def read(node: YNode): Either[YError, YScalar] = node.value match {
       case s: YScalar => Right(s)
-      case _          => error(node, "Yaml scalar expected")
+      case _          => error(node, "YAML scalar expected")
     }
     override def defaultValue: YScalar = YScalar.Null
   }
@@ -177,7 +177,7 @@ object YRead {
   implicit object SeqNodeYRead extends YRead[Seq[YNode]] {
     def read(node: YNode): Either[YError, Seq[YNode]] = node.value match {
       case s: YSequence => Right(s.nodes)
-      case _            => error(node, "Yaml sequence expected")
+      case _            => error(node, "YAML sequence expected")
     }
     override def defaultValue: Seq[YNode] = IndexedSeq.empty
   }
@@ -188,7 +188,7 @@ object YRead {
   implicit object YMapYRead extends YRead[YMap] {
     def read(node: YNode): Either[YError, YMap] = node.value match {
       case m: YMap => Right(m)
-      case _       => error(node, "Yaml map expected")
+      case _       => error(node, "YAML map expected")
     }
     override def defaultValue: YMap = YMap.empty
   }
@@ -199,7 +199,7 @@ object YRead {
   implicit object YSeqYRead extends YRead[YSequence] {
     def read(node: YNode): Either[YError, YSequence] = node.value match {
       case s: YSequence => Right(s)
-      case _            => error(node, "Yaml sequence expected")
+      case _            => error(node, "YAML sequence expected")
     }
     override def defaultValue: YSequence = YSequence.empty
   }
