1064bbfc1adad23dfe504f7f0661617d2228dfb6
Hernan Najles <hernan.naj+github@gmail.com>
2020-07-28 13:21:29 -0300

    Improve sequence recovery to handle same indentation that entry father

--- shared/src/main/scala/org/yaml/lexer/YamlLexer.scala
+++ shared/src/main/scala/org/yaml/lexer/YamlLexer.scala
@@ -1303,7 +1303,7 @@ final class YamlLexer private (input: LexerInput, positionOffset: Position = Pos
 
   // advance with error until sequence char: errorsUntilSeq
   // if not - is detected, advance until the indentation is the same that the father (n+1?) and go out. Father map recovery
-  private def errorUntilSeq(n:Int, m:Int) = oneOrMoreErrorSeqEntries(n,m,() => !looksLikeSeq(n,m)) && looksLikeSeq(n,m) && emitError()
+  private def errorUntilSeq(n:Int, m:Int) = oneOrMoreErrorSeqEntries(n,m,() => (!looksLikeSeq(n,m) && input.countSpaces()> n+m)) && looksLikeSeq(n,m) && emitError()
 
   private def errorsUntilBack(n:Int, m:Int) = oneOrMoreErrorSeqEntries(n,m, () => seqIndentedFromFather(n)) && emitError() || !nonEof
 
