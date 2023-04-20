9353f48c131b9dda43d71ebab5c83ddfd2febc5e
Hernan Najles <hernan.naj+github@gmail.com>
2023-04-20 10:43:44 -0300

    Try using chatAt of string instead of charsequence

--- shared/src/main/scala/org/mulesoft/lexer/CharSequenceLexerInput.scala
+++ shared/src/main/scala/org/mulesoft/lexer/CharSequenceLexerInput.scala
@@ -18,6 +18,7 @@ class CharSequenceLexerInput private (val data: CharSequence,
                                       override val sourceName: String)
     extends LexerInput {
 
+  private val stringData = data.toString
   private var state = InputState(offset = startOffset, nextOffset = startOffset).init(data, endOffset)
 
   /** The index of the character relative to the beginning of the line, as a 16 bit java character. (0 based) */
@@ -80,7 +81,7 @@ class CharSequenceLexerInput private (val data: CharSequence,
     var off = state.nextOffset + offset - 1
     if (off < 0) return 0
     while (i < max && off < endOffset) {
-      if (data.charAt(off)!= ' ') return i
+      if (stringData.charAt(off) != ' ') return i
       i = i + 1
       off += 1
     }
