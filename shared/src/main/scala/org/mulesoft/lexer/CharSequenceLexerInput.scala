59a5064db4090887b60132b97887929586377a76
Tomas Fernandez <tfernandez@mulesoft.com>
2023-04-24 16:10:48 -0300

    (chore): added comment to avoid modyfing the jdk 17 change

--- shared/src/main/scala/org/mulesoft/lexer/CharSequenceLexerInput.scala
+++ shared/src/main/scala/org/mulesoft/lexer/CharSequenceLexerInput.scala
@@ -18,6 +18,7 @@ class CharSequenceLexerInput private (val data: CharSequence,
                                       override val sourceName: String)
     extends LexerInput {
 
+  /** DO NOT CHANGE THIS. THIS IS AN OBSCURE HACK TO RUN IN JDK 17 (NO JOKE) */
   private val stringData = data.toString
   private var state = InputState(offset = startOffset, nextOffset = startOffset).init(data, endOffset)
 
@@ -81,6 +82,7 @@ class CharSequenceLexerInput private (val data: CharSequence,
     var off = state.nextOffset + offset - 1
     if (off < 0) return 0
     while (i < max && off < endOffset) {
+      /** DO NOT CHANGE THIS. THIS IS AN OBSCURE HACK TO RUN IN JDK 17 (NO JOKE) */
       if (stringData.charAt(off) != ' ') return i
       i = i + 1
       off += 1
